cc79a5b8a488928fdec3cb1ea94f8883b2724058
Roman Yermilov <roman.yermilov@globallogic.com>
2023-02-02 18:58:01 +0400

    Source Zendesk Support: fix flakecheck

--- airbyte-integrations/connectors/source-zendesk-support/source_zendesk_support/ZendeskSupportAvailabilityStrategy.py
+++ airbyte-integrations/connectors/source-zendesk-support/source_zendesk_support/ZendeskSupportAvailabilityStrategy.py
@@ -1,6 +1,3 @@
-import logging
-from typing import Optional, Tuple
-
 import requests
 
 from airbyte_cdk.sources.streams.http.availability_strategy import HttpAvailabilityStrategy
@@ -8,13 +5,10 @@ from airbyte_cdk.sources.streams.http.availability_strategy import HttpAvailabil
 
 class ZendeskSupportAvailabilityStrategy(HttpAvailabilityStrategy):
 
-    def check_availability(
-            self, stream: "source_zendesk_support.streams.SourceZendeskSupportStream",
-            logger: logging.Logger,
-            source: Optional["Source"]
-    ) -> Tuple[bool, Optional[str]]:
+    def check_availability(self, stream, logger, source):
         try:
-            stream.get_api_records_count()
+            get_api_records_count = getattr(stream, "get_api_records_count")
+            get_api_records_count()
         except requests.HTTPError as error:
             return self.handle_http_error(stream, logger, source, error)
         return True, None
